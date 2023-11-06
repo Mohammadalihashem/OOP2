b1777df7fee97cab762543ad2d053be6a51733ce
unknown <aboali.231996@gmail.com>
2023-11-07 02:23:26 +0300

    Abstraction2

---
+++ Abstraction/src/A.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class A implements speaker{
+    public void m1 (){
+        System.out.println("hello from a");
+            }
+    public void hello(){
+        System.out.println("hello from a agen");
+    }
+}
---
+++ Abstraction/src/B.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class B extends A{
+    public void m2(){
+        System.out.println("hello from b");
+    }
+}
